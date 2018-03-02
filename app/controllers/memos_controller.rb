4d0982ab53015193bfab63a70c7123299561bc9f
Daeun Yim <deyim33@gmail.com>
2018-03-02 16:16:31 +0900

    memo controller create

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,11 +2,17 @@ class MemosController < ApplicationController
 	#Create
   def new
   	@memo = Memo.new
+  	@memo.title = params[:memo][:title]
+  	@memo.content = params[:memo][:content]
+  	@memo.user_id = params[:memo][:user_id]
+  	@memo.save
+
+  	redircet_to @memo
   end
 
  # Read
   def show
-  	
+
   end
 
 #
